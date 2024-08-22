feaed049af3455694bcbb2fbef9f0822dca9318b
Bob Park <hwpark@malgn.com>
2024-08-22 11:22:20 +0900

    Initialize project

---
+++ index.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "printWidth": 80,
+    "semi": true,
+    "singleQuote": true,
+    "trailingComma": "all",
+    "tabWidth": 2,
+    "bracketSpacing": true,
+    "endOfLine": "auto",
+    "useTabs": false,
+    "importOrder": [
+        "^react",
+        "^next(/.*)$",
+        "@/hooks/(.*)$",
+        "@/utils/(.*)$",
+        "@/shared/(.*)$",
+        "@/entries/(.*)$",
+        "<THIRD_PARTY_MODULES>",
+        "^[./]"
+    ],
+    "importOrderSeparation": true,
+    "importOrderSortSpecifiers": true,
+    "plugins": [
+        "@trivago/prettier-plugin-sort-imports"
+    ]
+}
