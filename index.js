1d4d2f55568ca3b306bb4d86860410039cc5e2be
Bob Park <hwpark@malgn.com>
2024-08-23 09:35:35 +0900

    feat: tailwind css prettier plugin 추가 적용

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ module.exports = {
     "importOrderSeparation": true,
     "importOrderSortSpecifiers": true,
     "plugins": [
-        "@trivago/prettier-plugin-sort-imports"
+        "@trivago/prettier-plugin-sort-imports",
+        "prettier-plugin-tailwindcss"
     ]
 }
